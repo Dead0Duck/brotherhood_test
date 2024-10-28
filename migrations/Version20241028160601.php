f0fee54e7e627e6a197c5585b7e62a3d823c1d68
Dead0Duck <46423255+Dead0Duck@users.noreply.github.com>
2024-10-28 23:09:34 +0700

    ✨ Сущности Developer и Project

---
+++ migrations/Version20241028160601.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241028160601 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE developer (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL, job VARCHAR(255) NOT NULL, email VARCHAR(255) DEFAULT NULL, phone VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE project (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL, client VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE project_developer (project_id INT NOT NULL, developer_id INT NOT NULL, PRIMARY KEY(project_id, developer_id))');
+        $this->addSql('CREATE INDEX IDX_74C7CE4D166D1F9C ON project_developer (project_id)');
+        $this->addSql('CREATE INDEX IDX_74C7CE4D64DD9267 ON project_developer (developer_id)');
+        $this->addSql('ALTER TABLE project_developer ADD CONSTRAINT FK_74C7CE4D166D1F9C FOREIGN KEY (project_id) REFERENCES project (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE project_developer ADD CONSTRAINT FK_74C7CE4D64DD9267 FOREIGN KEY (developer_id) REFERENCES developer (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE project_developer DROP CONSTRAINT FK_74C7CE4D166D1F9C');
+        $this->addSql('ALTER TABLE project_developer DROP CONSTRAINT FK_74C7CE4D64DD9267');
+        $this->addSql('DROP TABLE developer');
+        $this->addSql('DROP TABLE project');
+        $this->addSql('DROP TABLE project_developer');
+    }
+}
---
+++ src/Entity/Project.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProjectRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProjectRepository::class)]
+class Project
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    /**
+     * @var Collection<int, Developer>
+     */
+    #[ORM\ManyToMany(targetEntity: Developer::class, inversedBy: 'projects')]
+    private Collection $team;
+
+    #[ORM\Column(length: 255)]
+    private ?string $client = null;
+
+    public function __construct()
+    {
+        $this->team = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Developer>
+     */
+    public function getTeam(): Collection
+    {
+        return $this->team;
+    }
+
+    public function addTeam(Developer $team): static
+    {
+        if (!$this->team->contains($team)) {
+            $this->team->add($team);
+        }
+
+        return $this;
+    }
+
+    public function removeTeam(Developer $team): static
+    {
+        $this->team->removeElement($team);
+
+        return $this;
+    }
+
+    public function getClient(): ?string
+    {
+        return $this->client;
+    }
+
+    public function setClient(string $client): static
+    {
+        $this->client = $client;
+
+        return $this;
+    }
+}
